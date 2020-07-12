0b0d933bf1858677a35848774d44d6a0595eac22
Sheeraz Ali <sheerazali656@gmail.com>
2020-07-11 19:48:07 -0700

    Initial Commit

---
+++ Remember_Me_Application/Remember_Me_Application/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace Remember_Me_Application
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxUsername = new System.Windows.Forms.TextBox();
+            this.textBoxPassword = new System.Windows.Forms.TextBox();
+            this.checkBoxRemeberMe = new System.Windows.Forms.CheckBox();
+            this.buttonBogin = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(50, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Username:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(50, 104);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Password:";
+            // 
+            // textBoxUsername
+            // 
+            this.textBoxUsername.Location = new System.Drawing.Point(114, 53);
+            this.textBoxUsername.Name = "textBoxUsername";
+            this.textBoxUsername.Size = new System.Drawing.Size(205, 20);
+            this.textBoxUsername.TabIndex = 0;
+            // 
+            // textBoxPassword
+            // 
+            this.textBoxPassword.Location = new System.Drawing.Point(114, 97);
+            this.textBoxPassword.Name = "textBoxPassword";
+            this.textBoxPassword.Size = new System.Drawing.Size(205, 20);
+            this.textBoxPassword.TabIndex = 1;
+            this.textBoxPassword.UseSystemPasswordChar = true;
+            // 
+            // checkBoxRemeberMe
+            // 
+            this.checkBoxRemeberMe.AutoSize = true;
+            this.checkBoxRemeberMe.Location = new System.Drawing.Point(114, 142);
+            this.checkBoxRemeberMe.Name = "checkBoxRemeberMe";
+            this.checkBoxRemeberMe.Size = new System.Drawing.Size(95, 17);
+            this.checkBoxRemeberMe.TabIndex = 2;
+            this.checkBoxRemeberMe.Text = "Remember Me";
+            this.checkBoxRemeberMe.UseVisualStyleBackColor = true;
+            // 
+            // buttonBogin
+            // 
+            this.buttonBogin.Location = new System.Drawing.Point(147, 179);
+            this.buttonBogin.Name = "buttonBogin";
+            this.buttonBogin.Size = new System.Drawing.Size(115, 36);
+            this.buttonBogin.TabIndex = 3;
+            this.buttonBogin.Text = "Login";
+            this.buttonBogin.UseVisualStyleBackColor = true;
+            this.buttonBogin.Click += new System.EventHandler(this.buttonBogin_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(393, 267);
+            this.Controls.Add(this.buttonBogin);
+            this.Controls.Add(this.checkBoxRemeberMe);
+            this.Controls.Add(this.textBoxPassword);
+            this.Controls.Add(this.textBoxUsername);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "REMEMBER ME APPLICATION";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxUsername;
+        private System.Windows.Forms.TextBox textBoxPassword;
+        private System.Windows.Forms.CheckBox checkBoxRemeberMe;
+        private System.Windows.Forms.Button buttonBogin;
+    }
+}
+
---
+++ Remember_Me_Application/Remember_Me_Application/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Remember_Me_Application
+{
+    public partial class Form1 : Form
+    {
+        string cs = ConfigurationManager.ConnectionStrings["csdb"].ConnectionString;
+
+        public Form1()
+        {
+            InitializeComponent();
+            LoadCredentials();
+        }
+
+        private void buttonBogin_Click(object sender, EventArgs e)
+        {
+            //if (textBoxUsername.Text == "Saba" && textBoxPassword.Text == "Mehak")
+            //{
+
+            //    MessageBox.Show("Login Successful!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            //    SaveCredentials();
+            //    this.Hide();
+            //    Form2 form2 = new Form2();
+            //    form2.Show();
+            //}
+            //else
+            //{
+
+            //    MessageBox.Show("Login Failed.", "Failure", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            //}
+
+            SqlConnection conn = new SqlConnection(cs);
+            string query = "select * from Login where Username = @uName and Password = @pass";
+            SqlCommand cmd = new SqlCommand(query, conn);
+            cmd.Parameters.AddWithValue("@uName", textBoxUsername.Text);
+            cmd.Parameters.AddWithValue("@pass", textBoxPassword.Text);
+
+            conn.Open();
+            
+            SqlDataReader reader = cmd.ExecuteReader();
+
+            if (!string.IsNullOrEmpty(textBoxUsername.Text) && !string.IsNullOrEmpty(textBoxPassword.Text))
+            {
+
+                if (reader.HasRows)
+                {
+
+                    MessageBox.Show("Login Successful!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    SaveCredentials();
+                    this.Hide();
+                    Form2 form2 = new Form2();
+                    form2.Show();
+                }
+                else
+                {
+
+                    MessageBox.Show("Invalid username or password", "Failure", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else {
+
+                MessageBox.Show("username or password is empty", "Failure", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            conn.Close();
+        }
+
+        private void SaveCredentials() {
+
+            if (checkBoxRemeberMe.Checked == true)
+            {
+
+                Properties.Settings.Default.Username = textBoxUsername.Text;
+                Properties.Settings.Default.Password = textBoxPassword.Text;
+                Properties.Settings.Default.Save();
+            }
+            else {
+
+                Properties.Settings.Default.Username = "";
+                Properties.Settings.Default.Password = "";
+                Properties.Settings.Default.Save();
+            }
+        }
+
+        private void LoadCredentials() {
+
+            if (Properties.Settings.Default.Username != string.Empty) {
+
+                textBoxUsername.Text = Properties.Settings.Default.Username;
+                textBoxPassword.Text = Properties.Settings.Default.Password;
+                checkBoxRemeberMe.Checked = true;
+            }
+        }
+    }
+}
